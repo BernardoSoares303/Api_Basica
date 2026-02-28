1be7480a1fea7ebf3bc43b6a47d71b3f3639f765
BernardoSoares303 <bbnado34@gmail.com>
2026-02-28 13:41:05 -0300

    Swagger adicionado

--- Api_Basica/Program.cs
+++ Api_Basica/Program.cs
@@ -1,18 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Basica V1");
+    c.RoutePrefix = string.Empty; // abre direto na raiz
+});
 
 app.UseHttpsRedirection();
 
